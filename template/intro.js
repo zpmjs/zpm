e95bb1113f6bbce9f5101a9d0079788c42c05568
hotoo <hotoo.cn@gmail.com>
2014-07-25 15:54:28 +0800

    Update template

--- template/intro.js
+++ template/intro.js
@@ -1,28 +1,29 @@
 
 (function(global){
 
+  var g_define = global.define;
   var zpm_define;
 
-  if (typeof define === "function" && (define.cmd || define.amd)){
-    zpm_define = define;
+  if (typeof g_define === "function" && (g_define.cmd || g_define.amd)){
+    zpm_define = g_define;
   } else {
 
-    var UMD_CACHE = "_zpm_cache_";
-    if (!global[UMD_CACHE]){
-      global[UMD_CACHE] = {};
+    var ZPM_CACHE = "_ZPM_CACHE_";
+    if (!global[ZPM_CACHE]){
+      global[ZPM_CACHE] = {};
     }
 
     var module = {
       exports: {},
       require: function(id){
-        return global[UMD_CACHE][id];
+        return global[ZPM_CACHE][id];
       }
     };
 
     zpm_define = function(id, deps, factory){
       var return_exports = factory.call(global, module.require, module.exports, module);
 
-      global[UMD_CACHE][id] = return_exports || module.exports;
+      global[ZPM_CACHE][id] = return_exports || module.exports;
     };
     zpm_define.zmd = true;
   }
