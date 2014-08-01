86bcd8bfdd497b7e48ce3b9db2dc2c08b803bca1
hotoo <hotoo.cn@gmail.com>
2014-08-01 14:37:28 +0800

    Add zpm.use()

--- template/intro.js
+++ template/intro.js
@@ -3,9 +3,15 @@
 
   var g_define = global.define;
   var zpm_define;
+  var zpmjs;
 
   if (typeof g_define === "function" && (g_define.cmd || g_define.amd)){
     zpm_define = g_define;
+    zpmjs = global.seajs || {
+      use: function(modules, callback){
+        global.require(modules, callback);
+      }
+    };
   } else {
 
     var ZPM_CACHE = "_ZPM_CACHE_";
@@ -28,6 +34,24 @@
       global[ZPM_CACHE][id] = return_exports || module.exports;
     };
     zpm_define.zmd = true;
+
+    zpmjs = {
+      use: function(modules, callback){
+        var args = [];
+
+        if (!(modules instanceof Array)){
+          modules = [modules];
+        }
+
+        for(var i=0,l=modules.length; i<l; i++){
+          args[i] = module.require(modules[i]);
+        }
+
+        callback.apply(global, args);
+      }
+    };
   }
+
+  global.zpmjs = zpmjs;
 
   zpm_define("{MODULE_ID}", "{MODULE_DEPS}", function(require, exports, module){
