4c335e2b7e3a6a194b56e09e4ff01b2ba4c87b2a
hotoo <hotoo.cn@gmail.com>
2014-08-28 16:45:16 +0800

    fixed for duplicate modules

--- tests/main.js
+++ tests/main.js
@@ -1,22 +1,9 @@
 
-var detector = require("detector");
-var ready = require("ready");
-var Events = require("evt");
-var nameStorage = require("name-storage");
 var Tracker = require("tracker");
 
 var T = {};
-
-nameStorage.setItem("ref", document.referrer);
-var evt = new Events();
-
-ready(function(){
-  evt.emit("ready");
-})
-
 T.click = function(){
   Tracker.click.apply(T, arguments)
-  return detector.os.name + "/" + detector.os.version;
 };
 
 module.exports = T;
